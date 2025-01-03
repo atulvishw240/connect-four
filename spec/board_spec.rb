57b72358b68442a8bf0f0664d7567aea65292b8a
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-03 17:45:23 +0530

    Add test for Board#display_row

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,4 +5,16 @@ describe Board do
   before(:each) do
     @board = Board.new
   end
+
+  describe "#display_row" do
+    it "takes a row from our board array as input and displays it" do
+      $stdout = StringIO.new
+      @board.display
+      $stdout.rewind
+
+      # expect($stdout.gets).to eq("-----------------------------\n")
+      expect($stdout.gets).to eq("| \u{26AB} | \u{26AB} | \u{26AB} | \u{26AB} | \u{26AB} | \u{26AB} | \u{26AB} |\n")
+      expect($stdout.gets).to eq("-----------------------------\n")
+    end
+  end
 end
