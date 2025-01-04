c6dfc0860ac746f89bbf21d542119fcbaffcf943
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-04 11:49:22 +0530

    Create game.rb file

---
+++ spec/game_spec.rb
@@ -0,0 +1,20 @@
+require_relative "../lib/board"
+require_relative "../lib/player"
+require_relative "../lib/game"
+
+describe do
+
+  before(:each) do
+    @board = Board.new
+    @player1 = Player.new("Atul", "\u{1F534}")
+    @player2 = Player.new("Gaurav", "\u{1F535}")
+    @game = Game.new(board, player1, player2)
+  end
+
+  describe "#current_player" do
+    it "returns @player1 because @player1 will get the first turn in every game" do
+
+      expect(@game.current_player).to eq(@player1)
+    end
+  end
+end
